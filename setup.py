dd5af6fa36d061fcceab6510d76050713cef7de3
XiangyangLi <946971688@qq.com>
2024-06-17 19:18:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="coir",
-    version="0.1.0",
+    name="coir-eval",
+    version="0.5.0",
     author="xiangyang Li",
     author_email="xiangyangli@pku.edu.cn",
     description="A package for COIR evaluations",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/yourusername/coir",
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests*']),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     install_requires=[
         "datasets>=2.19.0",
         "numpy>=1.0.0",
@@ -26,7 +26,7 @@ setup(
         "torch>1.0.0",
         "tqdm>1.0.0",
         "rich>=0.0.0",
-        "pytrec-eval-terrier>=0.5.6",
-        "beir"
+        "faiss_cpu",
+        "pytrec-eval-terrier>=0.5.6"
     ],
 )
