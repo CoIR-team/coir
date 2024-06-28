7d3b094400765bb4da8397d84ffb78946d2467a0
XiangyangLi <946971688@qq.com>
2024-06-28 11:53:02 +0800

    Update evaluation.py

--- coir/evaluation.py
+++ coir/evaluation.py
@@ -7,26 +7,26 @@ from coir.beir.retrieval.search.dense import DenseRetrievalExactSearch as DRES
 logger = logging.getLogger(__name__)
 
 class COIR:
-    def __init__(self, tasks):
+    def __init__(self, tasks, bacth_size):
         self.tasks = tasks
-
+        self.bacth_size = bacth_size
     def run(self, model, output_folder: str):
         results = {}
         for task_name, task_data in self.tasks.items():
             corpus, queries, qrels = task_data
 
             # Initialize custom model
-            custom_model = DRES(model, batch_size=128)
+            custom_model = DRES(model, batch_size=self.bacth_size)
             retriever = EvaluateRetrieval(custom_model, score_function="cos_sim")
 
             # Retrieve results
             task_results = retriever.retrieve(corpus, queries)
 
             # Evaluate results
-            ndcg, _map, recall, precision = retriever.evaluate(qrels, task_results, retriever.k_values)
+            ndcg, map, recall, precision = retriever.evaluate(qrels, task_results, retriever.k_values)
             metrics = {
                 "NDCG": ndcg,
-                "MAP": _map,
+                "MAP": map,
                 "Recall": recall,
                 "Precision": precision
             }
